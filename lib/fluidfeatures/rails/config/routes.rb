d9d8cb6b01c292500eafeca99cb9d9cd672aa17d
Phil Whelan <phil123@gmail.com>
2012-10-30 20:18:11 -0700

    remove "rails" directory from ruby generic gem

--- lib/fluidfeatures/rails/config/routes.rb
+++
@@ -1,8 +0,0 @@
-
-require 'fluidfeatures/rails/app/controllers/fluidfeatures_controller'
-
-Rails.application.routes.draw do
-  match "/fluidfeature/:feature_name"         => "fluid_feature_ajax#index"
-  match "/fluidgoal/:goal_name"               => "fluid_goal_ajax#index"
-  match "/fluidgoal/:goal_name/:goal_version" => "fluid_goal_ajax#index"
-end
