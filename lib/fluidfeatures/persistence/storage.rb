10450ed9f3c1ed9f54c2f62e4d75946c679fd6f3
Phil Whelan <phil123@gmail.com>
2013-01-11 11:35:56 -0800

    use @var instead of self.var

--- lib/fluidfeatures/persistence/storage.rb
+++ lib/fluidfeatures/persistence/storage.rb
@@ -6,9 +6,9 @@ module FluidFeatures
       attr_accessor :dir, :file_name, :store, :logger
 
       def initialize(config, logger=nil)
-        self.dir = config["dir"]
-        self.logger = logger || Logger.new(STDERR)
-        self.file_name = "#{self.class.to_s.split('::').last.downcase}.pstore"
+        @dir = config["dir"]
+        @logger = logger || Logger.new(STDERR)
+        @file_name = "#{self.class.to_s.split('::').last.downcase}.pstore"
         FileUtils.mkpath(dir) unless Dir.exists?(dir)
       end
 
